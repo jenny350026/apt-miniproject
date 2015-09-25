7eef9ea6bef6b286c2d27a0f6ad97861b54f2188
gges5110 <gges5110@gmail.com>
2015-09-24 22:44:30 -0500

    change identity to email

--- connexus.py
+++ connexus.py
@@ -15,7 +15,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 	)
 
 class Stream(ndb.Model):
-	author = ndb.StringProperty()
+	author_email = ndb.StringProperty()
 	name = ndb.StringProperty()
 	tag = ndb.StringProperty()
 	cover_url = ndb.StringProperty()	
@@ -24,6 +24,7 @@ class Image(ndb.Model):
 	stream = ndb.StructuredProperty(Stream)
 	date = ndb.DateTimeProperty(auto_now_add=True)
 	image = ndb.BlobProperty()
+	comment = ndb.StringProperty()
 
 class Subscriber(ndb.Model):
 	stream = ndb.StructuredProperty(Stream)
@@ -56,24 +57,28 @@ class MainPage(webapp2.RequestHandler):
 		
 class Manage(webapp2.RequestHandler):
 	def get(self):
+		streams = Stream().query(Stream.author_email == users.get_current_user().email())
+		template_values = {
+		'streams' : streams
+		}
 		template = JINJA_ENVIRONMENT.get_template('/templates/manage.html')
-		self.response.write(template.render({}))
+		self.response.write(template.render(template_values))
 
 class Create(webapp2.RequestHandler):
 	def post(self):
 		stream = Stream()
 		if users.get_current_user():
 			user = users.get_current_user()
-			stream.author = user.nickname()
+			stream.author_email = user.email()
 		else:
-			stream.author = 'Default User'
+			stream.author_email = 'Default User'
 
 		stream.name = self.request.get('stream_name')
 		stream.tag = self.request.get('tags')
 		stream.cover_url = self.request.get('cover_url')
 		stream.put()
 
-		for email in re.split('\s+,\s+', self.request.get('receipients')):
+		for email in re.split('\s*,\s*', self.request.get('receipients')):
 			subscriber = Subscriber()
 			subscriber.stream = stream
 			subscriber.email = email
