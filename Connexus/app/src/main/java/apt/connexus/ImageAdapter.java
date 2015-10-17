acdb2a23011b2f4c291f8ccb6973bed630ee767e
gges5110 <gges5110@gmail.com>
2015-10-16 21:05:49 -0500

    view single stream

--- Connexus/app/src/main/java/apt/connexus/ImageAdapter.java
+++ Connexus/app/src/main/java/apt/connexus/ImageAdapter.java
@@ -7,6 +7,9 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
@@ -39,12 +42,21 @@ public class ImageAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else {
+            imageView = (ImageView) convertView;
+        }
 
-        imageView = new ImageView(mContext);
-        imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        ImageLoader imageLoader = ImageLoader.getInstance();
+//        imageLoader.init(ImageLoaderConfiguration.createDefault(mContext));
+//        imageLoader.displayImage("http://apt-miniproject-1078.appspot.com/img?img_id=ahZzfmFwdC1taW5pcHJvamVjdC0xMDc4chILEgVJbWFnZRiAgICA-Ja1CAw.png", imageView);
+//
 
         Log.v(TAG, imageURLs.get(position));
+//        Picasso.with(mContext).load("http://apt-miniproject-1078.appspot.com/img?img_id=ahZzfmFwdC1taW5pcHJvamVjdC0xMDc4chILEgVJbWFnZRiAgICA-Ja1CAw").into(imageView);
         Picasso.with(mContext).load(imageURLs.get(position)).into(imageView);
         return imageView;
     }
